427283d2c24bcedba000d3f0bcefe74eecac1b69
John Kim <miki8435@colorado.edu>
2017-06-08 11:10:02 -0600

    Lab

--- Lab1/main.c
+++ Lab1/main.c
@@ -7,3 +7,43 @@ int main(void)
 {
 	return 0;
 }
+
+void blink(int n)
+
+char convert_to_morse(int n){
+    switch(n){
+    case 0:
+        return 0x11111;
+        break;
+    case 1:
+        return 0x01111;
+        break;
+    case 2:
+        return 0x11111;
+        break;
+    case 3:
+        print('dog');
+        break;
+    case 4:
+        print('dog');
+        break;
+    case 5:
+        print('dog');
+        break;
+    case 6:
+        print('dog');
+        break;
+    case 7:
+        print('dog');
+        break;
+    case 8:
+        print('dog');
+        break;
+    case 9:
+        print('dog');
+        break;
+    default:
+        print('wtf');
+        break;
+    }
+}
