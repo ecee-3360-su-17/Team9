2ef48302b863a361b9932709e3805bbf42a5d4fb
John Kim <miki8435@colorado.edu>
2017-06-20 11:38:02 -0600

    Merge to master (?)

--- Lab1 - Copy/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/ti/tiva-c-master/driverlib/ccs/Debug/driverlib.lib"
-
